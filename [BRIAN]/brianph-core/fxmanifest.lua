ac9d7e21dcd76b303e653ff67e96da81a32936e1
Brian <brianpasco1206@gmail.com>
2022-02-06 01:38:27 +0800

    Fixed and updated userCheckDatabase()

--- [BRIAN]/brianph-core/fxmanifest.lua
+++ [BRIAN]/brianph-core/fxmanifest.lua
@@ -5,5 +5,9 @@ game 'gta5'
 client_script 'client/cl_userHandler.lua'
 server_script 'server/sv_userHandler.lua'
 
+-- * OxMySQL connection
+server_script '@oxmysql/lib/MySQL.lua'
+
 -- * Dependencies
+
 dependency 'oxmysql'
