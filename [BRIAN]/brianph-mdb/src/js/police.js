f491fc1aa1b155dbefea99e259ca07b86a698b60
Brian <brianpasco1206@gmail.com>
2022-02-14 11:59:48 +0800

    [brianph-mdb] Finished police MDB navigation

---
+++ [BRIAN]/brianph-mdb/src/js/police.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    const navHighlight = $('.nav-buttons-highlight')
+
+    $('#dashboard').addClass('nav-buttons-active')
+    $('#dashboard').find(navHighlight).show()
+
+    $(document).on('click', '.nav-buttons', function() {
+
+        const navHighlight = $('.nav-buttons-highlight')
+        const navButton = $('.nav-buttons')
+
+        $(navButton).removeClass('nav-buttons-active')
+        $(navHighlight).hide()
+
+        $(this).addClass('nav-buttons-active')
+        $(this).find(navHighlight).show()
+
+    })
+
+})
