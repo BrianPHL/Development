7aaa79638c2fc1f2f5a52eafb5b41611ee994b64
Brian <brianpasco1206@gmail.com>
2022-02-28 11:56:15 +0800

    [brianph-mdb] Converted nav function to TS

--- [BRIAN]/brianph-mdb/src/js/main.js
+++ [BRIAN]/brianph-mdb/src/js/main.js
@@ -1,2 +1,18 @@
-const test = 'yes';
-console.log(test);
+$(() => {
+    //addEventListener "click" on navBtn
+    const navBtn = $('.nav-buttons');
+    // function when navBtn is clicked 
+    navBtn.click(function () {
+        // constanced variables
+        const navHighlight = $('.nav-buttons-highlight');
+        const navId = $(this).attr('id');
+        const navDiv = $('.content-child');
+        // hides previously activated styles
+        // it only shows on a clicked button
+        navDiv.hide();
+        navHighlight.hide();
+        // shows styles on clicked button 
+        $(this).find(navHighlight).show();
+        $('.' + navId).show();
+    });
+});
--- [BRIAN]/brianph-mdb/src/ts/main.ts
+++ [BRIAN]/brianph-mdb/src/ts/main.ts
@@ -1,3 +1,28 @@
-const test : string = 'yes'
+declare var jquery: any;
+declare var $: any;
 
-console.log(test)   
+$(() => {
+
+    //addEventListener "click" on navBtn
+    const navBtn = $('.nav-buttons')
+
+    // function when navBtn is clicked 
+    navBtn.click(function() {
+
+        // constanced variables
+        const navHighlight = $('.nav-buttons-highlight')
+        const navId        = $(this).attr('id')
+        const navDiv       = $('.content-child')
+
+        // hides previously activated styles
+        // it only shows on a clicked button
+        navDiv.hide()
+        navHighlight.hide()
+
+        // shows styles on clicked button 
+        $(this).find(navHighlight).show()
+        $('.' + navId).show()
+
+    })
+
+})
