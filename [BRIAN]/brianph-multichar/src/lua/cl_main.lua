fb23067626c5616470475dfc8ae2ca0190ec6df4
Brian <brianpasco1206@gmail.com>
2022-03-15 20:27:50 +0800

    added(func/ux): cursor pointer

--- [BRIAN]/brianph-multichar/src/lua/cl_main.lua
+++ [BRIAN]/brianph-multichar/src/lua/cl_main.lua
@@ -3,8 +3,10 @@ AddEventHandler('playerSpawned', function(isSpawned)
     if isSpawned then
 
         SendNUIMessage({type = 'stateObserver', isEnabled = true})
-        print('Prompted character selection to player')
+        SetNuiFocus(true, true)
 
+        print('Prompted character selection to player')
+        
     end
 
 end)
