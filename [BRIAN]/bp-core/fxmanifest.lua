a9b87fb63317c1a29b27c4e01fdce68713b21ffb
Brian <brianpasco1206@gmail.com>
2022-03-16 12:02:35 +0800

    added(modules): functions module

--- [BRIAN]/bp-core/fxmanifest.lua
+++ [BRIAN]/bp-core/fxmanifest.lua
@@ -4,16 +4,17 @@ game 'gta5'
 description 'bp-framework core resource'
 author 'BrianPHL'
 
+-- ? Modules !IMPORTANT
+server_script 'src/lua/sv_modules.lua'
+
 -- ? Connection
 client_script 'src/lua/connection/cl_main.lua'
 server_script 'src/lua/connection/sv_main.lua'
 
 -- ? Gameplay
-
 client_script 'src/lua/gameplay/cl_main.lua'
 server_script 'src/lua/gameplay/sv_main.lua'
 
 --? User
-
 client_script 'src/lua/user/cl_main.lua'
 server_script 'src/lua/user/sv_main.lua'
