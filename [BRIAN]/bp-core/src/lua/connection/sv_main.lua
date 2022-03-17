d49d33810c0e2466c6cc3300c061603e8c53aae3
Brian <brianpasco1206@gmail.com>
2022-03-17 22:33:08 +0800

    added(eventHandler): playerDropped

--- [BRIAN]/bp-core/src/lua/connection/sv_main.lua
+++ [BRIAN]/bp-core/src/lua/connection/sv_main.lua
@@ -4,6 +4,17 @@ RegisterServerEvent('bp-core:connection:playerSpawned')
 AddEventHandler('bp-core:connection:playerSpawned', function() 
     
     playerCount = playerCount + 1
-    print('Triggered server event:', 'bp-core:connection:playerSpawned') 
+    print(
+        'Triggered server event:', 'bp-core:connection:playerSpawned'
+    ) 
+
+end)
+
+AddEventHandler('playerDropped', function()
+
+    playerCount = playerCount - 1
+    print(
+        'Triggered event handler:', 'playerDropped'
+    )
 
 end)
