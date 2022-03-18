b9a1bcb0f2a9f6bd0b9d26235a2288e2e0374cb9
Brian <brianpasco1206@gmail.com>
2022-03-18 14:00:51 +0800

    fixed(fxmanifest.lua): oxmysql connection

--- [BRIAN]/bp-core/fxmanifest.lua
+++ [BRIAN]/bp-core/fxmanifest.lua
@@ -4,8 +4,10 @@ game 'gta5'
 description 'bp-framework core resource'
 author 'BrianPHL'
 
--- ? Modules !IMPORTANT
+-- ? Modules
+-- ! IMPORTANT; DO NOT REMOVE ELSE RESOURCE WILL BREAK
 server_script 'src/lua/sv_modules.lua'
+server_script '@oxmysql/lib/MySQL.lua'
 
 -- ? Connection
 client_script 'src/lua/connection/cl_main.lua'
@@ -19,10 +21,6 @@ server_script 'src/lua/gameplay/sv_main.lua'
 client_script 'src/lua/user/cl_main.lua'
 server_script 'src/lua/user/sv_main.lua'
 
--- ? OxMySQL connection
-
-server_script '@oxmysql/lib/MySQL.lua'
-
 -- ? Dependencies
 
-    dependency 'oxmysql'
+dependency 'oxmysql'
