f26507f5e27d8a4fc72782c9a7530557343d9d3d
Brian <brianpasco1206@gmail.com>
2022-03-26 22:38:42 +0800

    modified(bp-core): adjusted scripts for connection

--- [BRIAN]/bp-core/fxmanifest.lua
+++ [BRIAN]/bp-core/fxmanifest.lua
@@ -4,15 +4,20 @@ game 'gta5'
 description 'bp-framework core'
 author 'BrianPHL'
 
--- oxmysql
+-- Oxmysql
 server_script '@oxmysql/lib/MySQL.lua'
 
 -- BPX prefix
 client_script 'src/sh_bpx.lua'
 server_script 'src/sh_bpx.lua'
 
--- connection
-client_script 'src/connection/sh_*'
-server_script 'src/connection/sh_*'
+-- Connection handler
+server_script 'src/connection/sv_modules.lua'
+server_script 'src/connection/sv_main.lua'
+client_script 'src/connection/cl_main.lua'
 
+-- Dependencies
 dependency 'oxmysql'
+
+-- Provides
+provide 'core'
