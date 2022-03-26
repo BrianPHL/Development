0d48e2b525ea72fb87e330222e675ac4e0d64439
Brian <brianpasco1206@gmail.com>
2022-03-26 18:55:13 +0800

    added(bp-core): 'playerDropped' event handler

--- [BRIAN]/bp-core/src/connection/sh_main.lua
+++ [BRIAN]/bp-core/src/connection/sh_main.lua
@@ -7,5 +7,21 @@ BPX = {
     Queue      = {}
 }
 
+AddEventHandler('playerDropped', function()
 
+    local src        = source
+    local srcMatch   = BPX.Players[src]
+    local playerName = BPX.getNameIdentifier(src)
+
+    if srcMatch then
+
+        -- TODO: add a function that saves the player's info
+        -- TODO: last loc, bank bal, cash bal, items, & appearance
+
+        BPX.Players[src] = nil
+        print('[BPX] ' .. playerName .. ' quit or disconnected.')
+
+    end
+
+end)
 
